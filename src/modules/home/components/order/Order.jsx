5a777d9a0ba2f3a41c8590ede8711ddd517c7862
Hrishikesh Rajan <hrishikeshrajan3@gmail.com>
2024-06-14 14:26:02 +0530

    refactor: data order button

--- src/modules/home/components/order/Order.jsx
+++ src/modules/home/components/order/Order.jsx
@@ -6,9 +6,9 @@ import Card from "../card/Card";
 const Order = ({data}) => {
     if(!data) return null;
   return (
-    <Background url={data.card.images.baseImage} width={'10vh'} >
+    <Background url={data.card.images.baseImage} width={'20vh'} className='flex justify-center items-center' >
         <Card className='w-full flex justify-center items-center py-5'>
-        <button className="w-6/12 px-3  py-2 bg-[#0796EF] text-white">{data.card.title}</button>
+        <button className="w-6/12 md:w-4/12 xl:w-2/12 px-3  py-2 bg-[#0796EF] text-white">{data.card.title}</button>
         </Card>
     </Background>
      
