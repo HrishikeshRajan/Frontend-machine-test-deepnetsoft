049f4d624f5a3962382648804e85b25eb94f0c92
Hrishikesh Rajan <hrishikeshrajan3@gmail.com>
2024-06-14 14:22:50 +0530

    refactor: order info

--- src/modules/home/components/order/OrderInfo.jsx
+++ src/modules/home/components/order/OrderInfo.jsx
@@ -7,10 +7,10 @@ const OrderInfo = ({data}) => {
     if(!data) return null
   return (
 
-    <Background url={data.card.images.baseImage} width="50vh" className='p-5 '>
+    <Background url={data.card.images.baseImage} width="40vh" className='p-5 flex justify-center items-center '>
          <Card className='bg-[#665430]'>
-           <ul className="text-white text-xs font-light p-4">
-              {data.card.data.map((item) => <li className="text-center py-2" key={item.id}>{item.text}</li>)}
+           <ul className="flex flex-col xl:flex-row text-white text-xs font-light p-4">
+              {data.card.data.map((item,index) => <li className={`text-center py-2 px-5  ${index + 1 === data.card.data.length? 'xl:border-r-0' :'xl:border-r-2 '} ${index + 1 === data.card.data.length? 'border-b-0 border-r-0' :'border-b-2 border-r-0 '} py-5 xl:border-b-0  border-dotted`} key={item.id}>{item.text}</li>)}
            </ul>
          </Card>
     </Background>
