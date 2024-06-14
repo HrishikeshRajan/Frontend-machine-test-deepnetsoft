387bdc5becff385ffcbab71cf0536089c710e9bd
Hrishikesh Rajan <hrishikeshrajan3@gmail.com>
2024-06-14 16:17:55 +0530

    navbar bug updated

--- src/modules/home/components/navbar/Navbar.jsx
+++ src/modules/home/components/navbar/Navbar.jsx
@@ -13,10 +13,10 @@ const Navbar = ({data}) => {
   return (
     <Card  className={'w-full h-[100px] top-[-25px] gap-0 opacity-100 bg-[#121618] relative'}>
       <header className="flex justify-center xl:justify-start" >
-         <Image src={card.image.url} className={'absolute w-[80px] xl:w-[120px] h-[70px] xl:h-[90px] object-[-6%] top-[64px] xl:top-[53px]  xl:left-[50px] gap-0 opacity-100 object-cover xl:object-[-12%]'} alt={card.image.altText} />
-         <Heading  className={'absolute w-[154px] h-[52px] top-[60px]  text-4xl font-[Oswald] left-[170px] gap-0 opacity-100 object-cover object-left-[-10%] hidden lg:block'}>{card.title.additionalStyles.map((val,i) => <span key={i} style ={{...val.style}}>{val.text}</span>)}</Heading>
+         <Image src={card.image.url} className={'absolute w-[80px] xl:w-[120px] h-[70px] xl:h-[90px] object-[-6%] top-[64px] xl:top-[53px]  xl:left-[50px] gap-0 opacity-100 object-cover xl:object-[-12%] lg:hidden'} alt={card.image.altText} />
+         <Heading  className={'absolute w-[154px] h-[52px] top-[60px]  text-4xl font-[Oswald] left-[170px] gap-0 opacity-100 object-cover object-left-[-10%] hidden xl:block'}>{card.title.additionalStyles.map((val,i) => <span key={i} style ={{...val.style}}>{val.text}</span>)}</Heading>
       </header>
-      <Navs navs={card.navs.list} className={'hidden lg:block'} />
+      <Navs navs={card.navs.list} className={'hidden xl:block'} />
       <button className="absolute top-[55px] right-0 xl:hidden pe-5"><Image src={card.navs.button.icon} /></button>
     </Card>
   )
